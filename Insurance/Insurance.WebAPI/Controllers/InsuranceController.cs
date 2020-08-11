d3d6329ca7d149dac653ecb5d70ebe444a0d21b0
Juan Pablo Alvis <jpan_2009@hotmail.com>
2020-08-11 10:12:36 -0500

    Add Web API project

---
+++ Insurance/Insurance.WebAPI/Controllers/InsuranceController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Insurance.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InsuranceController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Works");
+        }
+    }
+}
