2430b4fe1ef87cde1060a2eca8b8bc7174a69360
Juan Pablo Alvis <jpan_2009@hotmail.com>
2020-08-13 11:37:22 -0500

    Remove unused usings

--- Insurance/Insurance.Repository/CustomerRepository.cs
+++ Insurance/Insurance.Repository/CustomerRepository.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Internal;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 
 namespace Insurance.Repository
 {
--- Insurance/Insurance.WebAPI/Services/CustomerService.cs
+++ Insurance/Insurance.WebAPI/Services/CustomerService.cs
@@ -1,7 +1,6 @@
 ﻿using Insurance.Core.Exceptions;
 using Insurance.Core.Models;
 using Insurance.Repository;
-using Insurance.WebAPI.Models;
 using System.Collections.Generic;
 using System.Linq;
 
