a2330b79e6fa4ba7b986ef9a5b520613ce8ef302
Juan Pablo Alvis <jpan_2009@hotmail.com>
2020-08-13 11:36:17 -0500

    Refactor repository GetById method

--- Insurance/Insurance.Repository/CustomerRepository.cs
+++ Insurance/Insurance.Repository/CustomerRepository.cs
@@ -68,38 +68,35 @@ namespace Insurance.Repository
 
         public CustomerModel GetById(int id)
         {
-            var insurancesByCustomer = (from ci in _context.CustomerInsurances
-                                        join c in _context.Customers
-                                        on ci.CustomerId equals c.Id
-                                        join i in _context.Insurances.Include(c => c.InsurancesCoverages)
-                                        on ci.InsuranceId equals i.Id
-                                        where c.Id == id
-                                        select new
-                                        {
-                                            Customer = c,
-                                            Insurance = i
-                                        }).ToList();
+            var customerEntity = _context.Customers.SingleOrDefault(c => c.Id == id);
 
-            var customer = (from d in insurancesByCustomer
-                            group d by d.Customer into g
-                            select new CustomerModel
-                            {
-                                Id = g.Key.Id,
-                                Name = g.Key.Name,
-                                Insurances = g.Select(d => new InsuranceModel
-                                {
-                                    Id = d.Insurance.Id,
-                                    Name = d.Insurance.Name,
-                                    Description = d.Insurance.Description,
-                                    CoverageRate = d.Insurance.CoverageRate,
-                                    StartDate = d.Insurance.StartDate,
-                                    MonthsOfCoverage = d.Insurance.MonthsOfCoverage,
-                                    Price = d.Insurance.Price,
-                                    Risk = (Risk)d.Insurance.RiskId,
-                                    CoverageTypes = d.Insurance.InsurancesCoverages.Where(ic => ic.InsuranceId == d.Insurance.Id)
-                                                 .Select(ic => (CoverageType)ic.CoverageTypeId)
-                                })
-                            }).FirstOrDefault();
+            if (customerEntity is null)
+                return null;
+
+            var insurancesByCustomer = from ci in _context.CustomerInsurances
+                                       where ci.CustomerId == customerEntity.Id
+                                       join i in _context.Insurances.Include(i => i.InsurancesCoverages)
+                                       on ci.InsuranceId equals i.Id
+                                       select i;
+
+            var customer = new CustomerModel
+            {
+                Id = customerEntity.Id,
+                Name = customerEntity.Name,
+                Insurances = insurancesByCustomer.Select(i => new InsuranceModel
+                {
+                    Id = i.Id,
+                    Name = i.Name,
+                    Description = i.Description,
+                    CoverageRate = i.CoverageRate,
+                    StartDate = i.StartDate,
+                    MonthsOfCoverage = i.MonthsOfCoverage,
+                    Price = i.Price,
+                    Risk = (Risk)i.RiskId,
+                    CoverageTypes = i.InsurancesCoverages.Where(ic => ic.InsuranceId == i.Id)
+                                 .Select(ic => (CoverageType)ic.CoverageTypeId)
+                })
+            };
 
             return customer;
         }
