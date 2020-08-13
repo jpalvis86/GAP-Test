d684066c10a5eee9ed18bceefe990811f36854d4
Juan Pablo Alvis <jpan_2009@hotmail.com>
2020-08-13 15:12:55 -0500

    Add Home Component and set basic routing

--- Insurance/Insurance-UI/src/app/app-routing.module.ts
+++ Insurance/Insurance-UI/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { InsurancesComponent } from './components/insurances/insurances.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'customers', component: CustomersComponent },
+  { path: 'insurances', component: InsurancesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Insurance/Insurance-UI/src/app/app.module.ts
+++ Insurance/Insurance-UI/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InsurancesComponent } from './components/insurances/insurances.component';
 import { CustomersComponent } from './components/customers/customers.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, InsurancesComponent, CustomersComponent],
+  declarations: [AppComponent, InsurancesComponent, CustomersComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
