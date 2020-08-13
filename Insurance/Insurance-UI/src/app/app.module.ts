ec696ea678a30d31f997d4fc469ab09819f1086b
Juan Pablo Alvis <jpan_2009@hotmail.com>
2020-08-13 14:25:17 -0500

    Add main components

--- Insurance/Insurance-UI/src/app/app.module.ts
+++ Insurance/Insurance-UI/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InsurancesComponent } from './components/insurances/insurances.component';
+import { CustomersComponent } from './components/customers/customers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InsurancesComponent,
+    CustomersComponent
   ],
   imports: [
     BrowserModule,
