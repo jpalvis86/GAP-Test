38b72b1ba7e39948697473b6e1ce85c6865dee53
Juan Pablo Alvis <jpan_2009@hotmail.com>
2020-08-14 15:07:49 -0500

    Improve exception messages

--- Insurance/Insurance.Core/Exceptions/InsuranceStartDateIsNotValidException.cs
+++ Insurance/Insurance.Core/Exceptions/InsuranceStartDateIsNotValidException.cs
@@ -5,7 +5,7 @@ namespace Insurance.Core.Exceptions
     public class InsuranceStartDateIsNotValidException : Exception
     {
         public InsuranceStartDateIsNotValidException(DateTime startDate)
-            : base($"Insurance Start Date: {startDate.ToString("dd/MM/yyyy")} is not valid.")
+            : base($"Insurance Start Date: {startDate.ToString("dd/MM/yyyy")} is not valid. It must be in the future")
         {
 
         }
